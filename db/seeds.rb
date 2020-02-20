02f24f719afffbf9a43ead221912ccac1a738317
Cerian Smith <ceri.smith@yahoo.de>
2020-02-20 16:37:48 +0000

    Done without front-end sadness

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+
+Cocktail.create(name: "Gin&Tonic")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "salt")
